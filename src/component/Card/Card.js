05cc8e3e9b8c6224434773611ed90f8b1079b5a5
gayatrisathawane <gayatripsathawane@gmail.com>
2023-09-18 11:51:15 +0530

    incr and dec button added

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -1,14 +1,38 @@
+import { useState } from 'react';
 import Button from './../Button/Button';
 import './Card.css';
 
+
 export default function Card({pname,price,img,size})
 {
+  const [count,setCount]=useState(0)
+
+  function incr(){
+   
+    setCount(count+1)
+
+   
+  }
+  function dec(){
+   
+    setCount(count-1)
+
+    if(count===0)
+    {
+     
+    }
+
+   
+  }
+
     return(
        <div className='card'>
          <img src={img} className='img'/>
         <p className="pname">{pname}</p>
         <h4 className="price">price :{price}     <span className="offer"> offer  50% </span></h4>
-        <p className='size'>{size}</p>
+        <p className='size'>{size}</p><br/>
+      <button type='button' className='btncount' onClick={incr} > + </button>  <span className='count'>{count}</span>
+        <button type='button' className='btncount' onClick={dec}>-</button><br/>
         <Button text="Buy now"/>
        
        </div>
